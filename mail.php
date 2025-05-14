4ff654cff65d06c99cc0eb0c1b4cf96580aa4105
Adhyatmika Eka Saputra <148543848+Dehayeka@users.noreply.github.com>
2025-05-14 15:55:42 +0700

    test

--- mail.php
+++ mail.php
@@ -3,7 +3,7 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         # FIX: Replace this email with recipient email
-        $mail_to = "PUT YOUR EMAIL ADDRESS";
+        $mail_to = "ddhykaa@gmail.com";
         
         # Sender Data
         $subject = trim($_POST["subject"]);
